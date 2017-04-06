0c99924b0cc4732d42d278956d87d5fc8751bc84
btekse <btekse@gmail.com>
2017-04-06 15:00:04 +0200

    file(app) : add Main

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import static com.sun.org.apache.xalan.internal.xsltc.cmdline.Compile.printUsage;
+
+/**
+ * Created by Balázs on 2017. 04. 06..
+ */
+public class Main {
+  public static void main(String[] args) {
+
+    if (args.length == 0) {
+
+    System.out.println("Java Todo application\n" +
+      "=======================\n" +
+      "Command line arguments:\n" +
+      " -l   Lists all the tasks\n" +
+      " -a   Adds a new task\n" +
+      " -r   Removes an task\n" +
+      " -c   Completes an task");
+    else 
+    }
+
+  }
+}
